857736e3607f16a1d8dd21e58390f667fd639f78
Faisal Ahamed <faisal.hye@gmail.com>
2020-11-17 20:00:53 +0600

    splash screen sample

---
+++ splashScreen sample/android/app/src/main/kotlin/com/example/splashScreen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.splashScreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ splashScreen sample/lib/screens/dashboard/dashboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welocme to Dashboard'),
+      ),
+      body: Text('dashboard'),
+    );
+  }
+}
