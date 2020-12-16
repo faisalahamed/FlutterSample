fd95bb319bf1dad8db68832ea935c9d5a76bbd7e
faisalahamed <faisal.hye@gmail.com>
2020-12-16 18:42:36 +0600

    update tab mobile\

--- mobile_tab_desktop_basic/lib/views/home/home_desktop.dart
+++ mobile_tab_desktop_basic/lib/views/home/home_desktop.dart
@@ -4,8 +4,19 @@ class HomeDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.all(15),
       color: Colors.black38,
-      child: Text('Home Desktop'),
+      child: GridView.count(
+        crossAxisCount: 4,
+        children: List.generate(50, (index) {
+          return Container(
+            height: 50,
+            child: Card(
+              color: Colors.blue[100],
+            ),
+          );
+        }),
+      ),
     );
   }
 }
